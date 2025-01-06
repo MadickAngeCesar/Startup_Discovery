9ac028bc22ffa1ece4eb631abf08359b69455cfb
MadickAngeCesar <madick.angecesar@ictuniversity.edu.cm>
2025-01-06 12:33:52 +0100

    refactor(auth): enhance GitHub authentication flow with user creation and session management

--- auth.ts
+++ auth.ts
@@ -88,6 +88,7 @@ export const authOptions: NextAuthOptions = {
 export const getAuthSession = () => getServerSession(authOptions)
 export const handler = NextAuth(authOptions)*/
 
+/*
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import { getServerSession } from "next-auth/next";
@@ -147,3 +148,57 @@ export { authHandler as GET, authHandler as POST };
 export const auth = async () => await getServerSession(options);
 export const signIn = authSignIn;
 export const signOut = authSignOut;
+*/
+
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import { AUTHOR_BY_GITHUB_ID_QUERY } from "@/sanity/lib/queries";
+import { client } from "@/sanity/lib/client";
+import { writeClient } from "@/sanity/lib/write-client";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  providers: [GitHub],
+  callbacks: {
+    async signIn({
+      user: { name, email, image },
+      profile: { id, login, bio },
+    }) {
+      const existingUser = await client
+        .withConfig({ useCdn: false })
+        .fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
+          id,
+        });
+
+      if (!existingUser) {
+        await writeClient.create({
+          _type: "author",
+          id,
+          name,
+          username: login,
+          email,
+          image,
+          bio: bio || "",
+        });
+      }
+
+      return true;
+    },
+    async jwt({ token, account, profile }) {
+      if (account && profile) {
+        const user = await client
+          .withConfig({ useCdn: false })
+          .fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
+            id: profile?.id,
+          });
+
+        token.id = user?._id;
+      }
+
+      return token;
+    },
+    async session({ session, token }) {
+      Object.assign(session, { id: token.id });
+      return session;
+    },
+  },
+});
