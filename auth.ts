fb68662f2614e464ad0cc70e9bcba2bdac458859
MadickAngeCesar <madick.angecesar@ictuniversity.edu.cm>
2025-01-06 06:10:04 +0100

    refactor(auth): simplify GitHub provider configuration in authentication options

--- auth.ts
+++ auth.ts
@@ -97,12 +97,7 @@ import { client } from "@/sanity/lib/client";
 import { writeClient } from "@/sanity/lib/write-client";
 
 const options = {
-  providers: [
-    GitHub({
-      clientId: process.env.AUTH_GITHUB_ID!,
-      clientSecret: process.env.AUTH_GITHUB_SECRET!,
-    }),
-  ],
+  providers: [GitHub({ clientId: process.env.GITHUB_CLIENT_ID, clientSecret: process.env.GITHUB_CLIENT_SECRET })],
   callbacks: {
     async signIn({ user, profile }) {
       const { id, login, bio } = profile;
